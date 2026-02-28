f2b04b1d8a1bbafab9a7a1e715d41a9c2510f163
mr.mewadi <kamleshpurbiya2004@gmail.com>
2026-02-28 17:49:41 +0530

    gitingone setup fix

--- core/urls.py
+++ core/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('accounts/', include('accounts.urls')),
 ] 
-if settings.DEBUG or not settings.DEBUG: 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
